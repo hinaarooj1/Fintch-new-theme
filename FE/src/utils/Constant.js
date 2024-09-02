9dc77518eaa44a56b98cdb4f774230d806c21e96
A. Developers <ahmarjabbar7@gmail.com>
2024-09-02 20:07:56 +0500

    hh

--- FE/src/utils/Constant.js
+++ FE/src/utils/Constant.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Local server
-  baseUrl: "http://localhost:4000/api/v1",
+  // baseUrl: "http://localhost:4000/api/v1",
   // Local server
   // Official server
-  // baseUrl: "https://fintcx-jzvg.onrender.com/api/v1",
+  baseUrl: "https://fintcx-jzvg.onrender.com/api/v1",
   // baseUrl: "https://fintch-server.onrender.com/api/v1",
   // Official server
 };
